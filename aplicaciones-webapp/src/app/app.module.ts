b9928e09a9e46b0648edb8b36c212ec59c40e577
EduardoSandovalZ <eduardo.sandovalz@alumno.buap.mx>
2023-09-05 21:30:12 -0600

    Update1

---
+++ aplicaciones-webapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Angular Material
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginScreenComponent,
+    RegistroScreenComponent,
+    HomeScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
