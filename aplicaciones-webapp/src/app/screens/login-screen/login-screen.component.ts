35798bec4355eca2b2c0364a483657ee42a80778
EduardoSandovalZ <eduardo.sandovalz@alumno.buap.mx>
2023-09-13 04:05:10 -0600

    falta la funcion de showPassword

--- aplicaciones-webapp/src/app/screens/login-screen/login-screen.component.ts
+++ aplicaciones-webapp/src/app/screens/login-screen/login-screen.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent {
+  type : String = "password";
+  
+  constructor(){}
+  ngOnInit(): void{
+
+  }
+  public login(){
+
+  }
+  public showPassword(){
+  }
 
 }
